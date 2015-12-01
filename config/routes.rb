bd7aed34c338e03250bce132156446a98a570887
Cassio Cabral <cassioscabral@gmail.com>
2015-11-30 21:52:30 -0300

    create route to report

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Spree::Core::Engine.routes.draw do
-  # Add your extension routes here
+  namespace :admin do
+    resources :reports, only: [:index] do
+      collection do
+        get :sales_total
+        post :sales_total
+        get :products_ran_out_of_stock
+        post :products_ran_out_of_stock
+      end
+    end
+  end
 end
